205c6b9a2023579a75e3380a953b8d6b3f5a3b10
thomas-fm <13519099@std.stei.itb.ac.id>
2021-05-11 05:07:43 +0700

    fixing bug on path construction

---
+++ speech_rec.py
@@ -0,0 +1,9 @@
+import librosa
+from dtw import dtw
+import librosa.display
+from scipy.spatial.distance import cdist
+
+# TO DO
+# Visualize the shape of signal
+# Visualize the shape of dtw
+# Do the machine learning shit
